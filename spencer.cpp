3d3c82c24069ed268f7f397090efb20092179cd1
root <root@COMP3004.(none)>
2013-03-12 17:49:48 -0400

    Spencers pseudocode

---
+++ spencer.cpp
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+void messageReceived(string messageData){
+    if(data is a message){
+        // check who it is for
+        // If it is me then yay
+        // If it is not look that person up in the table and forward it
+    }else if(data is a routing vector from a neighbour){
+        // update our routing table
+        // tell the neighbours about our routing table by constructing
+        // a vector of the shortest paths
+    }
+}
