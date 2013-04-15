99633a9ff09ce9b15c830fed3f3f6e584b5352cc
root <root@COMP3004.(none)>
2013-04-15 14:51:32 -0400

    Fixed a bug in John

--- NDP.h
+++ NDP.h
@@ -21,7 +21,7 @@ class NDP: public MessageHandler{
   // Constants
   static const int updateMinDelay = 100;
   static const int updateMaxDelay = 300;
-  static const int beaconDelay = 100;
+  static const int beaconDelay = 1000;
   static const int maxAge = 3;
 
 // Public Member Functions
